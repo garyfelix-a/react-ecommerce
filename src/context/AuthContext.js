6fee92caccb40f13b938487a2bf8c142aa237029
garyfelix23 <a.garyfelix@gmail.com>
2025-02-24 22:48:06 +0530

    Created context api

---
+++ src/context/AuthContext.js
@@ -0,0 +1,13 @@
+import React, { createContext } from "react";
+
+const AuthProvider = createContext();
+
+const AuthContext = () => {
+    return(
+        <div>
+            AuthContext
+        </div>
+    );
+}
+
+export default AuthContext;
